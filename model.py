78f69450e5765b316eafbfe5d606999ae903ab4a
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2023-12-20 03:12:04 +0900

    feat - EfficientNet과 Imgaug 추가 (#34)
    #32

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 import timm
+from transformers import AutoConfig, AutoModel
+
 
 class BaseModel(nn.Module):
     """
@@ -120,4 +122,91 @@ class sh_vit_model(nn.Module):
 
     def forward(self, x):
         x = self.vitmodel(x)
+        return x
+    
+class EfficientNet_b0(nn.Module): # input size 224 224
+    def __init__(self, num_classes):
+        super(EfficientNet_b0, self).__init__()
+
+        # Load EfficientNet model and its configuration
+        config = AutoConfig.from_pretrained('google/EfficientNet-b0')
+        self.eff_net = AutoModel.from_pretrained('google/EfficientNet-b0', config=config)
+
+        # Custom layers after EfficientNet
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # Adaptive pooling to get a fixed size output
+        self.dropout = nn.Dropout(0.1)  # Adding dropout for regularization
+        self.fc = nn.Linear(config.hidden_dim, num_classes)  # Custom fully connected layer
+
+    def forward(self, x):
+        # Pass input through EfficientNet
+        eff_net_output = self.eff_net(x)
+        
+        # Extract the output of EfficientNet
+        x = eff_net_output.last_hidden_state
+        x = F.relu(x)  # Adding ReLU activation
+        
+        # Additional custom layers
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)  # Flatten the output for the fully connected layer
+        x = self.dropout(x)  # Applying dropout
+        x = self.fc(x)
+
+        return x
+    
+class EfficientNet_b1(nn.Module): # input size 240 240
+    def __init__(self, num_classes):
+        super(EfficientNet_b1, self).__init__()
+
+        # Load EfficientNet model and its configuration
+        config = AutoConfig.from_pretrained('google/EfficientNet-b1')
+        self.eff_net = AutoModel.from_pretrained('google/EfficientNet-b1', config=config)
+
+        # Custom layers after EfficientNet
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # Adaptive pooling to get a fixed size output
+        self.dropout = nn.Dropout(0.1)  # Adding dropout for regularization
+        self.fc = nn.Linear(config.hidden_dim, num_classes)  # Custom fully connected layer
+
+    def forward(self, x):
+        # Pass input through EfficientNet
+        eff_net_output = self.eff_net(x)
+        
+        # Extract the output of EfficientNet
+        x = eff_net_output.last_hidden_state
+        x = F.relu(x)  # Adding ReLU activation
+        
+        # Additional custom layers
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)  # Flatten the output for the fully connected layer
+        x = self.dropout(x)  # Applying dropout
+        x = self.fc(x)
+
+        return x
+
+class EfficientNet_b2(nn.Module): # input size 260 260
+    def __init__(self, num_classes):
+        super(EfficientNet_b2, self).__init__()
+
+        # Load EfficientNet model and its configuration
+        config = AutoConfig.from_pretrained('google/EfficientNet-b2')
+        self.eff_net = AutoModel.from_pretrained('google/EfficientNet-b2', config=config)
+
+        # Custom layers after EfficientNet
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # Adaptive pooling to get a fixed size output
+        self.dropout = nn.Dropout(0.1)  # Adding dropout for regularization
+        self.fc = nn.Linear(self.eff_net.config.hidden_dim, num_classes)  # Custom fully connected layer
+
+    def forward(self, x):
+        # Pass input through EfficientNet
+        eff_net_output = self.eff_net(x)
+        
+        # Extract the output of EfficientNet
+        x = eff_net_output.last_hidden_state
+        x = F.relu(x)  # Adding ReLU activation
+        
+        # Additional custom layers
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)  # Flatten the output for the fully connected layer
+        x = self.dropout(x)  # Applying dropout
+        x = self.fc(x)
+
         return x
